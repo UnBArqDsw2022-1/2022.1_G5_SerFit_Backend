637450914628bf6219422925ce8fa8baec897380
SwampTG <adriansoareslps@gmail.com>
2022-07-05 23:23:55 -0300

    Config envs

--- api/config/config.js
+++ api/config/config.js
@@ -1,2 +1,17 @@
 var dotenv = require('dotenv');
 
+var enviroment = process.env.ENVIROMENT.toLowerCase();
+let envPath = __dirname + `/../.env.${enviroment}`;
+console.log(envPath);
+
+dotenv.config({path: envPath});
+
+var config = {
+	port: process.env.PORT,
+};
+
+console.log(config.port);
+
+module.exports = config;
+
+
