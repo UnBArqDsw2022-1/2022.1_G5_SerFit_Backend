dc3dbb9b61a7f364d2d0625b1f3623ce7ccdb965
Luis Gustavo <luis.gustavo3013@hotmail.com>
2022-07-27 10:08:43 -0300

    Created user model and sequelize configuration (migrations)
    Co-authored-by: Gustave Persijn <gpersijn@outlook.com>

---
+++ api/config/database.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+const environment = process.env.ENVIROMENT.toLowerCase();
+const envPath = __dirname + `/../.env.${environment}`;
+
+dotenv.config({ path: envPath });
+
+const db = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAMES,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: true
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAMES,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: true
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAMES,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT
+    }
+};
+
+module.exports = db;
---
+++ api/database/migrations/20220727125321-create-user.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const AccountType = require('../../utils/AccountType');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+      },
+      password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          validate: {
+              notEmpty: true,
+              len: [8, 150]
+          }
+      },
+      name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          validate: {
+              len: [2, 50],
+              notEmpty: true
+          }
+      },
+      accountType: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          isIn: [[AccountType.personal, AccountType.client]]
+      }
+      },
+      age: {
+          type: Sequelize.INTEGER,
+          allowNull: true
+      },
+      about: {
+          type: Sequelize.STRING,
+          allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
