ff44bea925f89fd5d9139ab336cbe2395f2372fb
Luis Gustavo <luis.gustavo3013@hotmail.com>
2022-08-05 15:26:30 -0300

    Added gym routes
    Co-authored-by: Gustave Persijn <gpersijn@outlook.com>

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,11 @@
 const accountRoutes = require('./users');
+const activityRoutes = require('./activity');
+const gymRoutes = require('./gym');
 
 module.exports = function (app) {
   app.use('/api', [
-    accountRoutes
+    accountRoutes,
+    activityRoutes,
+    gymRoutes
   ])
 };
