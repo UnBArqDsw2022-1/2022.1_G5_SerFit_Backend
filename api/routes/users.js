fb45f0d5c175d6c3f6f44df302280213f9466143
SwampTG <adriansoareslps@gmail.com>
2022-08-06 14:50:55 -0300

    #69 - Rota para todos os users
    Co-authored-by: Daniel Alves <DanielViniciusAlves@users.noreply.github.com>

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 const PATH = "/user/";
 
-router.post(PATH + "create/", UserController.create);
+router.get(`${PATH}`, UserController.all);
+/* router.get(`${PATH}find`, UserController.find); */
+router.post(`${PATH}create/`, UserController.create);
 
 module.exports = router;
