5a5d86b35880391b1ccdd40894476f81ade89182
SwampTG <adriansoareslps@gmail.com>
2022-08-06 16:08:45 -0300

    #69 - Método pesquisa por id
    Co-authored-by: Daniel Alves <DanielViniciusAlves@users.noreply.github.com>

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,28 +1,53 @@
+'use strict';
 const db = require('../models/index');
 const jwt = require('jsonwebtoken');
 
 class UserController {
-    async create(req, res) {
-        const { password, email, name, accountType } = req.body;
-        const user = { password, email, name, accountType };
-        try {
-            const account = await db.user.create(user);
-            const id = account.id;
-            const token = jwt.sign({ id }, process.env.SECRET, {
-                expiresIn: 30000
-            });
-            return res.status(201).json({ auth: true, token: token });
-        } catch (error) {
-            console.log(error);
-            return res.status(401).json(error);
-        }
+  constructor() {}
+  async create(req, res) {
+    const { password, email, name, accountType } = req.body;
+    const user = { password, email, name, accountType };
+    try {
+      const account = await db.user.create(user);
+      const id = account.id;
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 10000,
+      });
+      return res.status(201).json({ auth: true, token: token });
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json(error);
     }
+  }
 
-    async all(_req, res) {
-        return db.user.findAll()
-            .then(users => res.status(200).json(users))
-            .catch(err => res.status(400).json(err));
+  async all(_req, res) {
+    return db.user
+      .findAll()
+      .then((users) => res.status(200).json(users))
+      .catch((err) => res.status(400).json(err));
+  }
+
+  async find(req, res) {
+    let queries = req.query;
+    if (queries.id) {
+      return this.findById(queries.id, res);
+    } else {
+      return this.compositeQuery(queries, res);
     }
+  }
+
+  findById(userId, res) {
+    return db.user
+      .findAll({
+        where: { id: userId },
+      })
+      .then((users) => res.status(200).json(users))
+      .catch((err) => res.status(400).json(err));
+  }
+
+  compositeQuery(_queries, res) {
+    return res.status(500).send('Not implemented');
+  }
 }
 
 module.exports = new UserController();
