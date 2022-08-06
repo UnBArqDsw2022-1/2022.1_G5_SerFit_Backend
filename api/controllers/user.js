a1141d68ed9a3601b0c95b8f9d92fb2919d1bf74
SwampTG <adriansoareslps@gmail.com>
2022-08-06 14:48:36 -0300

    #69 - Retorna todos os users
    Co-authored-by: Daniel Alves <DanielViniciusAlves@users.noreply.github.com>

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,8 +2,6 @@ const db = require('../models/index');
 const jwt = require('jsonwebtoken');
 
 class UserController {
-    constructor() { }
-
     async create(req, res) {
         const { password, email, name, accountType } = req.body;
         const user = { password, email, name, accountType };
@@ -18,7 +16,13 @@ class UserController {
             console.log(error);
             return res.status(401).json(error);
         }
-     }
+    }
+
+    async all(_req, res) {
+        return db.user.findAll()
+            .then(users => res.status(200).json(users))
+            .catch(err => res.status(400).json(err));
+    }
 }
 
 module.exports = new UserController();
