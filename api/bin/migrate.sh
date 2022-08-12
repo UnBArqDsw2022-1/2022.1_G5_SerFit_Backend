bf73fa709f37f486eb213d25448c05c9ff045f39
SwampTG <adriansoareslps@gmail.com>
2022-08-11 23:18:57 -0300

    #69 - Configuração do Sequelize Docker

--- api/bin/migrate.sh
+++ api/bin/migrate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo docker exec -i serfit_api bash -c "export MIGRATED=1 sequelize db:create && \
-    sequelize db:migrate && \
-    sequelize db:seed"
+sudo docker exec -i serfit_api bash -c " MIGRATED=1 sequelize db:migrate"
