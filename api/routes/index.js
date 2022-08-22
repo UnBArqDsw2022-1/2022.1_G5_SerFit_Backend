41565d41f14ddb05a5a882422961c7dc458e1372
Daniel <daniel.alves@targetso.com>
2022-08-22 15:41:53 -0300

    #74 - Atualizando branch e corrigindo conflitos.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,21 @@
 const accountRoutes = require('./users');
+const activityRoutes = require('./activity');
+const exerciseCategoriesRoutes = require('./exercise-category');
+const exerciseRoutes = require('./exercise');
+const gymRoutes = require('./gym');
+const storeRoutes = require('./store');
+const userRoutes = require('./users');
+const loginRoutes = require('./login');
 
 module.exports = function (app) {
   app.use('/api', [
-    accountRoutes
+    accountRoutes,
+    activityRoutes,
+    exerciseCategoriesRoutes,
+    exerciseRoutes,
+    gymRoutes,
+    storeRoutes,
+    userRoutes,
+    loginRoutes
   ])
 };
