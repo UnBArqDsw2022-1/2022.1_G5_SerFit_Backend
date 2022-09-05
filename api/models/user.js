a3a0c89fdc3ae155abd31a95d8c9d30bac0233e9
SwampTG <adriansoareslps@gmail.com>
2022-09-05 20:01:35 -0300

    Fix Dockerfile e UserModel

--- api/models/user.js
+++ api/models/user.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         validate: {
           notEmpty: true,
-          isIn: INTERESTS,
+          isIn: [INTERESTS],
         },
       },
       age: {
